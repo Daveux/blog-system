9751fb126b8bb5b10a64cfeea38ddf451da35102
David Odeyinka <david.odeyinka@yahoo.com>
2019-03-26 13:27:56 +0100

    Inserted data into db, queried data and showed in the index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+let mongo = require('mongodb');
+let db = require('monk')('localhost/blog-system');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let db = req.db;
+  let posts = db.get('posts');
+  posts.find((posts, err) => {
+    res.render('index', { posts: posts });
+  });
+
 });
 
 module.exports = router;
