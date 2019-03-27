e11882f0ea4d35b80f704c97aca171df7a85f30c
David Odeyinka <david.odeyinka@yahoo.com>
2019-03-27 13:34:07 +0100

    Using tuts code for app.js

--- app.js
+++ app.js
@@ -1,98 +1,108 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let bodyParser = require('body-parser');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let session = require('express-session');
-let multer = require('multer');
-let upload = multer({ dest: 'uploads/' });
-let moment = require('moment');
-let expressValidator = require('express-validator');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+var expressValidator = require('express-validator');
 
-let mongo = require('mongodb');
-let db = require('monk')('localhost/nodeblog');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
 
-// let routes = require('./routes/index');
-// let users = require('./routes/users')
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
+var app = express();
 
-let app = express();
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Express Session
+// Express Session
 app.use(session({
     secret: 'secret',
     saveUninitialized: true,
     resave: true
 }));
 
-//Express Validator
+// Express Validator
 app.use(expressValidator({
     errorFormatter: function(param, msg, value) {
-      let namespace = param.split('.')
-          , root = namespace.shift()
-          , formParam = root;
-      while (namespace.length) {
-        formParam += '[' + namespace.shift() + ']';
-      }
-      return {
-        param: formParam,
-          msg: msg,
-          value: value
-      };
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
     }
 }));
-//Connect-Flash
-app.use(function(req, res, next){
-  res.locals.messages =require('express-messages')(req,res);
-  next();
+
+
+// Connect-Flash
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-app.use(function(req, res, next){
-  req.db = db;
-  console.log(req);
-  next();
-});
-app.get('/', function(req, res, next) {
-    let db = req.db;
-    // console.log(db);
-    let posts = db.get('posts');
-    posts.find({}, {}, (posts, err) => {
-        res.render('index', { posts: posts });
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
+}
 
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
